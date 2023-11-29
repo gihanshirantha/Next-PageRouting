fd3c4b0c02c2fdbdb5e4e1bdc900161a7d771d68
Gihan Shirantha <shirantha1999@gmail.com>
2023-11-29 13:08:51 +0530

    patch: Change heading elements with propper html syntax

--- ui-core/components/atoms/title/Title.tsx
+++ ui-core/components/atoms/title/Title.tsx
@@ -4,7 +4,7 @@ import { HeadingProps } from "./Title.types";
 export const H1: React.FC<HeadingProps> = ({ title }) => {
   return (
     <>
-      <p className="font-bold text-3xl">{title}</p>
+      <h1 className="font-bold text-3xl">{title}</h1>
     </>
   );
 };
@@ -12,7 +12,7 @@ export const H1: React.FC<HeadingProps> = ({ title }) => {
 export const H2: React.FC<HeadingProps> = ({ title }) => {
   return (
     <>
-      <p className="font-semibold text-xl">{title}</p>
+      <h2 className="font-semibold text-xl">{title}</h2>
     </>
   );
 };
@@ -20,7 +20,7 @@ export const H2: React.FC<HeadingProps> = ({ title }) => {
 export const H3: React.FC<HeadingProps> = ({ title }) => {
   return (
     <>
-      <p className="font-medium text-xl">{title}</p>
+      <h3 className="font-medium text-xl">{title}</h3>
     </>
   );
 };
